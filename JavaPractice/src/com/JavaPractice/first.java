91cc151f7dda09027c61f0173b2eeaa66092e626
dhara <dhara@gmail>
2021-07-16 11:53:59 +0530

    solved dfgvdf

--- JavaPractice/src/com/JavaPractice/first.java
+++ JavaPractice/src/com/JavaPractice/first.java
@@ -3,6 +3,9 @@ package com.JavaPractice;
 public class first {
 	public static void main(String a[]) {
 		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
 	}
 
 }
