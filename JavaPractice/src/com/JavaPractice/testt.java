44cea93e002791dab3242843be33c1959b29f14c
dhara <dhara@gmail>
2021-07-16 12:27:01 +0530

    new

---
+++ JavaPractice/src/com/JavaPractice/testt.java
@@ -0,0 +1,18 @@
+package com.JavaPractice;
+
+public class testt {
+	
+	public static void main(String a[]) {
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
+	}
+}
