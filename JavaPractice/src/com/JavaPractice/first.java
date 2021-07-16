c3c093627224ace30a090aed06c05adbe58bc960
dhara <dhara@gmail>
2021-07-16 11:51:03 +0530

    solved

--- JavaPractice/src/com/JavaPractice/first.java
+++ JavaPractice/src/com/JavaPractice/first.java
@@ -2,7 +2,7 @@ package com.JavaPractice;
 
 public class first {
 	public static void main(String a[]) {
-		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!!!!!!!!1");
+		  System.out.print("hello good mornhhhhhhhhhhhhhhhhhhhhhhhhhhhing!!!,jjjjjjjjjj!!!!!!!1");
 	}
 
 }
